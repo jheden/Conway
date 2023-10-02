3f138c7f9ae0f2cbefc2b26870d056b7e3008ebb
Jonathan Hedén <jonathan.heden@gmail.com>
2023-10-02 14:22:28 +0200

    Adds controls for zoom and simulation speed

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,21 +2,31 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    private int frameRate = 0;
+    private int minFrameRate = 10;
     private int maxFrameRate = 200;
+    private int frameRateStep = 10;
+    private int minZoom = 1;
+    private int maxZoom = 40;
+    private float zoomStep = 0.1f;
+    public Grid grid;
+
+    private void Start()
+    {
+        Application.targetFrameRate = 50;
+    }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            frameRate = (frameRate + 25) % maxFrameRate;
-            Application.targetFrameRate = frameRate;
-        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            Application.targetFrameRate = Mathf.Max(minFrameRate, Application.targetFrameRate - frameRateStep);
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            frameRate = (frameRate - 25 + maxFrameRate) % maxFrameRate;
-            Application.targetFrameRate = frameRate;
-        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            Application.targetFrameRate = Mathf.Min(maxFrameRate, Application.targetFrameRate + frameRateStep);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            Camera.main.orthographicSize = Mathf.Max(minZoom, Camera.main.orthographicSize - zoomStep);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            Camera.main.orthographicSize = Mathf.Min(maxZoom, Camera.main.orthographicSize + zoomStep);
     }
 }
--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -35,7 +35,7 @@ public abstract class Grid : MonoBehaviour
             UpdateVertices();
         }
     }
-    protected Vector2 _size = new(20, 20);
+    protected Vector2 _size = new(10, 10);
     #endregion
 
     #region Internal variables
